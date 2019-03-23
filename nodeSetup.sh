cba22c162279d9f733d4689809b3a123b8017c31
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2019-03-23 18:21:52 -0400

    Compiles release explicitly now

--- nodeSetup.sh
+++ nodeSetup.sh
@@ -2,11 +2,10 @@ sudo apt-get install pkg-config
 git clone https://github.com/input-output-hk/jormungandr --recursive
 cd jormungandr/cardano-deps/imhamt/src/ && rm -f bitmap.rs &&
 wget https://raw.githubusercontent.com/input-output-hk/rust-cardano/e1d08491988f01cda6cec1aa019226f555791f66/imhamt/src/bitmap.rs
-cd ../../.. && cargo build &&
-cd target/debug/ &&
+cd ../../.. && cargo build --release &&
+cd target/release/ &&
 cp jormungandr ../../../node/ &&
 cp jormungandr_cli ../../../node/ &&
 cd ../../../node/ &&
 ./jormungandr generate-priv-key --type=Ed25519Extended > private.key &&
 (cat private.key | ./jormungandr generate-pub-key) > public.key
-
