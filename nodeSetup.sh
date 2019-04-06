da8a33506cd83d565b07d5c3aa05ae9cd5f203d0
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2019-04-06 12:00:24 -0400

    Also install requirements now

--- nodeSetup.sh
+++ nodeSetup.sh
@@ -1,11 +1,15 @@
-sudo apt-get install pkg-config
+sudo apt-get install curl pkg-config
+curl https://sh.rustup.rs -sSf | sh
+rustup install stable
+rustup default stable
 git clone https://github.com/input-output-hk/jormungandr --recursive
 cd jormungandr/cardano-deps/imhamt/src/ && rm -f bitmap.rs &&
 wget https://raw.githubusercontent.com/input-output-hk/rust-cardano/e1d08491988f01cda6cec1aa019226f555791f66/imhamt/src/bitmap.rs
 cd ../../.. && cargo build --release &&
 cd target/release/ &&
 cp jormungandr ../../../node/ &&
-cp jormungandr_cli ../../../node/ &&
+cp jcli ../../../node/ &&
 cd ../../../node/ &&
 ./jormungandr generate-priv-key --type=Ed25519Extended > private.key &&
 (cat private.key | ./jormungandr generate-pub-key) > public.key
+jcli genesis init | jcli genesis encode --output block_0.bin
